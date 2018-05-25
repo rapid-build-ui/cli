4c8d0861b72f73719cc5ae3c254e5bb0d30a5304
Judd Younce <jyounce@yahoo.com>
2018-05-24 23:55:58 -0500

    feat(travis): components.release complete

--- src/server/index.js
+++ src/server/index.js
@@ -6,10 +6,15 @@ require('./bootstrap/colors');
 const Utils = {
 	travis: {
 		components: {
-			async continuous(config) { // :Promise{}
+			async continuous(config) { // :Promise<any>
 				const steps = require('./travis/component-steps');
 				await steps.buildComponent(config.paths);
 				await steps.triggerShowcaseBuild(config.tokens.travis, config.repoName);
+			},
+
+			async release(config) { // :Promise<any>
+				const steps = require('./travis/component-steps');
+				await steps.buildComponent(config.paths);
 			}
 		}
 	}
